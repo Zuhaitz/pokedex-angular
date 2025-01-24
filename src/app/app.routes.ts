5c1dd9e5030b9e528887efc840d6cf948ecd4fbf
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-01-24 15:02:34 +0100

    navbar fix and new page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 
 import { BaseLayoutComponent } from './layouts';
-import { HomeComponent } from './pages';
+import { AboutComponent, HomeComponent } from './pages';
 
 export const routes: Routes = [
   {
     path: '',
     component: BaseLayoutComponent,
-    children: [{ path: '', component: HomeComponent }],
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+    ],
   },
   {
     path: '**',
