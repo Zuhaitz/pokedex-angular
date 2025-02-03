c4c7bac38d0d3c4889547844818a1dbf06ee0181
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-02-03 15:17:12 +0100

    Auto-generated commit

--- chunk-S7KQMKZV.js
+++
@@ -1,1 +0,0 @@
-import{A as d,B as e,w as a,x as b,y as c}from"./chunk-J3TX6OHZ.js";export{c as FooterComponent,a as NavLinkComponent,b as NavbarComponent,e as PokedexComponent,d as PokemonCardComponent};
