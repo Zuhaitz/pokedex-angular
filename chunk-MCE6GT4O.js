69e6863fea7d791bfbd34b3379748bffddf977ec
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-02-06 15:15:33 +0100

    Auto-generated commit

--- chunk-MCE6GT4O.js
+++
@@ -1,1 +0,0 @@
-import{A as b,B as c,D as d,E as e,z as a}from"./chunk-PLQS2EBD.js";export{c as FooterComponent,a as NavLinkComponent,b as NavbarComponent,e as PokedexComponent,d as PokemonCardComponent};
